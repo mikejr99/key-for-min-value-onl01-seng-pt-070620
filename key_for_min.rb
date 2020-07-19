06bd4272f5a0b1178ee1eaca285028005757ef67
Miguel Felix <mikeyfelix99@gmail.com>
2020-07-19 09:41:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   key = nil
   
   name_hash.each do |name, num|
-    
+    binding.pry
     if checknum = 0 || num < checknum
       checknum = num
       key = name
